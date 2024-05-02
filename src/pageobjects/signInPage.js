6d11c01c02c35c9d81a73e560710435a65a4acf5
Kristina Garina <Kristina.Garina@coxautoinc.com>
2024-05-02 14:55:06 +0300

    remove line

--- src/pageobjects/signInPage.js
+++ src/pageobjects/signInPage.js
@@ -10,9 +10,6 @@ class SignInPage {
   usernameInput = "//input[@id='username']";
   passwordInput = "//input[@id='password']";
 
-  authUser = users[0].username;
-  authPassword = "s3cret";
-
   async login() {
     const usernameElement = await getElement(this.usernameInput);
     const passwordElement = await getElement(this.passwordInput);
